4231578e53c9387c2426343997c16c0cecda69dd
Javier <javierpalomares100@hotmail.com>
2024-09-21 16:16:48 -0500

    added comment test1 vscode

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def JpApp():
 def list_pc():
   return jsonify(PC)
 
-#test->first git commit from VSCode
+#test1->first git commit from VSCode
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
